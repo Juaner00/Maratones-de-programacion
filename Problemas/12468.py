25d02607c0fcbea4e48946e70c42150784c2a33c
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-08-05 20:40:00 -0500

    Create 12468.py

---
+++ Problemas/12468.py
@@ -0,0 +1,12 @@
+def Main():
+	n1, n2 = int(input()), int(input())
+	while (n1 != -1 and n2 != -1):
+		menor = min(n1, n2)
+		mayor = max(n1, n2)
+		if (mayor - menor > 100 - mayor + menor):
+			print(100 - mayor + menor)
+		else:
+			print(mayor - menor)
+		n1, n2 = int(input()), int(input())
+
+Main()
