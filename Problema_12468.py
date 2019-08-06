1d4072bdd7b08260ff7dbe0209d5745a533fb62f
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-08-05 20:27:19 -0500

    Update Problema_12468.py

--- Problema_12468.py
+++ Problema_12468.py
@@ -1,1 +1,10 @@
-test
+def Main():
+	s1, s2, s3 = int(input()), int(input()), int(input())
+	while (s1 != 0 and s2 != 0 and s3 != 0):
+		if (s1**2 + s2**2 == s3**2):
+			print("right")
+		else:
+			print("wrong")
+		s1, s2, s3 = int(input()), int(input()), int(input())
+
+Main()
