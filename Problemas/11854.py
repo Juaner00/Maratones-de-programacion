bc9fd9b22b2296cd1336b2d5a05939513d91ca61
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-08-06 10:49:19 -0500

    Update 11854.py

--- Problemas/11854.py
+++ Problemas/11854.py
@@ -1,10 +1,10 @@
 def Main():
-	s1, s2, s3 = int(input()), int(input()), int(input())
+	s1, s2, s3 = map(int, input().split())
 	while (s1 != 0 and s2 != 0 and s3 != 0):
 		if (s1**2 + s2**2 == s3**2):
 			print("right")
 		else:
 			print("wrong")
-		s1, s2, s3 = int(input()), int(input()), int(input())
+		s1, s2, s3 = map(int, input().split())
 
 Main()
