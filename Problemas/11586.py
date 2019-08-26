0bb8dd5c3251c586e92d6b6c14fe04cb097a40d5
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-08-25 21:18:12 -0500

    Create 11586.py

---
+++ Problemas/11586.py
@@ -0,0 +1,23 @@
+""" LARGE
+for i in range(int(input())):
+	line = input()
+	contM = line.count('M')
+	contF = line.count('F')
+	f contM == contF and len(line) != 2:
+	  print("LOOP") i
+	else:
+		print("NO LOOP")	
+"""
+
+""" MEDIUM
+for i in range(int(input())):
+	line = input()
+	contM = line.count('M')
+	contF = line.count('F')
+	print("LOOP") if contM == contF and len(line) != 2 else print("NO LOOP")	
+"""	
+
+""" SHORT """
+for i in range(int(input())):
+	line = input()
+	print("LOOP") if line.count('M') == line.count('F') and len(line) != 2 else print("NO LOOP")		
