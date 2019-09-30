15b9c48b53ace9b53999c425cbe1ad130da9ce5c
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-09-30 18:07:36 -0500

    Create 554.py

---
+++ Problemas/554.py
@@ -0,0 +1,41 @@
+def main():
+	alf = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+	dic = []
+	line = input()
+	while line != '#':
+		dic.append(line)
+		line = input()
+
+	enc = []
+	for char in input():
+		enc.append(char)
+
+	indexOr = []
+	for ind, char in enumerate(enc):
+		indexOr.append(alf.find(char))
+
+	mayor = 0
+	shiftMayor = 0
+
+	for shift in range(1, len(alf)):
+		encCopy = enc
+		msg = ''.join(encCopy)
+
+		match = 0
+		for word in msg.split():
+			if word in dic:
+				match += 1
+			if match > mayor:
+				mayor = match 
+				shiftMayor = shift - 1
+
+		for index, char in enumerate(encCopy):
+			encCopy[index] = alf[indexOr[index] - shift]
+		
+	for index, char in enumerate(enc):
+		enc[index] = alf[indexOr[index] - shiftMayor]
+	
+	print(''.join(enc))
+	
+main()
