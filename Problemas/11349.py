1fc2d6b0b886f5ea5c2caafcc9f0272b4db281ad
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-09-30 18:10:55 -0500

    Update 11349.py

--- Problemas/11349.py
+++ Problemas/11349.py
@@ -1,42 +1,42 @@
 def main():
-    testCase = int(input())
+	alf = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    for x in range(testCase):
-        #Crear matriz
-        n = input().split()
-        matrix = []
-        for i in range(int(n[2])):
-            data = input().split()
-            matrix.append(data)
-            #matrix.append(input().split())
-        #print(matrix)
-        #Todos +
-        negativo = False
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                matrix[i][j] = int(matrix[i][j])
-                if matrix[i][j] < 0:
-                    print("Test #{}: Non-symmetric.".format(x + 1))
-                    negativo = True
-                    break
-            if negativo:
-                break
-        if negativo:
-            continue
+	dic = []
+	line = input()
+	while line != '#':
+		dic.append(line)
+		line = input()
 
-        #Simetrica
-        simetrico = True
-        for i in range(int(len(matrix) / 2) + 1):
-            for j in range(len(matrix)):
-                if matrix[i][j] != matrix[len(matrix) - 1 - i][len(matrix) - 1 - j]:
-                    simetrico = False
-                    break
-            if not simetrico:
-                break
-        if simetrico:
-            print("Test #{}: Symmetric.".format(x + 1))
-        else:
-            print("Test #{}: Non-symmetric.".format(x + 1))
+	enc = []
+	for char in input():
+		enc.append(char)
 
+	indexOr = []
+	for ind, char in enumerate(enc):
+		indexOr.append(alf.find(char))
+
+	mayor = 0
+	shiftMayor = 0
+
+	for shift in range(1, len(alf)):
+		encCopy = enc
+		msg = ''.join(encCopy)
+
+		match = 0
+		for word in msg.split():
+			if word in dic:
+				match += 1
+			if match > mayor:
+				mayor = match 
+				shiftMayor = shift - 1
+
+		for index, char in enumerate(encCopy):
+			encCopy[index] = alf[indexOr[index] - shift]
+		
+	for index, char in enumerate(enc):
+		enc[index] = alf[indexOr[index] - shiftMayor]
+	
+	print(''.join(enc), end="")
+	
 if __name__ == '__main__':
     main()
