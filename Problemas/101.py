88c5f87ad1cd30ec39e0f0ea16e5354651a8a9be
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-09-29 22:24:45 -0500

    Update 101.py

--- Problemas/101.py
+++ Problemas/101.py
@@ -1,143 +1,71 @@
-from getch import getche
+def simulation(act1, src, act2, tgt, piles, blocks):
+    """
+    piles: list of list
+    blocks, record the block in which pile
+    """
+    if src == tgt or blocks[src] == blocks[tgt]:
+        # src == tgt or src and tgt in the same pile
+        return piles, blocks
+
+    if act1 == 'move':
+        # pre-action, move the blocks above a to their original piles
+        for num in reversed(piles[blocks[src]]):
+            if num == src:
+                break
+            piles[num].append(piles[blocks[src]].pop())
+            blocks[num] = num
+
+    if act2 == 'onto':
+        # pre-action, move the blocks above b to their original piles
+        for num in reversed(piles[blocks[tgt]]):
+            if num == tgt:
+                break
+            piles[num].append(piles[blocks[tgt]].pop())
+            blocks[num] = num
+
+    if act1 == 'move':
+        # real action, put a on b, for both act2 in 'into' and 'over'
+        piles[blocks[tgt]].append(piles[blocks[src]].pop())
+        blocks[src] = blocks[tgt]
+
+    if act1 == 'pile':
+        # real action, move the blocks above a to b
+        src_loc = piles[blocks[src]].index(src)
+        orig_src = blocks[src]
+        for num in piles[blocks[src]][src_loc:]:
+            piles[blocks[tgt]].append(num)
+            blocks[num] = blocks[tgt]
+
+        for _ in range(len(piles[orig_src]) - src_loc):
+            piles[orig_src].pop()
+
+    return piles, blocks
+
 
 def main():
-	# Hace los bloques
-	blocks = [[i] for i in range(int(input()))]
+    while 1:
+        try:
+            n_block = int(input())
+            piles = [[v] for v in range(n_block)]
+            # record block in which one pile
+            blocks = [v for v in range(n_block)]
+            while 1:
+                cmd = input().split()
+                if cmd[0] == 'quit':
+                    for idx, block in enumerate(piles):
+                        if len(block):
+                            print("{}: {}".format(
+                                idx, " ".join(str(v) for v in block)))
+                        else:
+                            print("{}:".format(idx))
 
-	isQuit = False
-	while not isQuit:
-		msg = ""
-		while not isQuit:
-			line = getche()
-			msg += line
-			if msg[-4:] == 'quit':
-				isQuit = True
-				break
+                    break
+                act1, src, act2, tgt = cmd
+                piles, blocks = simulation(act1, int(src), act2, int(tgt),
+                                           piles, blocks)
+               
+        except (EOFError):
+            break
 
-		mA = msg.split()
-
-		print(mA)
-		for x in range(0, len(mA) - 4, 4):
-			if mA[x] == 'move' and mA[x + 2] == 'onto':
-				MoveOnto(mA[x + 1], mA[x + 3], blocks)
-			elif mA[x] == 'move' and mA[x + 2] == 'over':
-				MoveOver(mA[x + 1], mA[x + 3], blocks)
-			elif mA[x] == 'pile' and mA[x + 2] == 'onto':
-				PileOnto(mA[x + 1], mA[x + 3], blocks)
-			elif mA[x] == 'pile' and mA[x + 2] == 'over':
-				PileOver(mA[x + 1], mA[x + 3], blocks)
-
-	# Mostrar blocks
-	for i in range(len(blocks)):
-		for indJ, j in enumerate(blocks[i]):
-			if len(blocks[i]) > 0:
-				blocks[i][indJ] = str(j)
-
-	for indI, i in enumerate(blocks):
-		num = " ".join(i)
-		if len(num) > 0:
-			print(str(indI) + ": " + num)
-		else:
-			print(str(indI) + ":")
-
-def MoveOnto(y, x, blocks):
-	index = []
-	indexY = []
-	# Buscar a y b y quitar los del stack de a y b
-	for indI, i in enumerate(blocks):
-		if len(i) > 0:
-			for indJ, j in enumerate(blocks[indI]):
-				if j == int(x):
-					index = [indI, indJ]
-					for ind, k in enumerate(blocks[indI]):
-						if ind > indJ:
-							blocks[k].append(k)
-							blocks[indI][ind] = None
-				if j == int(y):
-					indexY = [indI, indJ]
-					for ind, k in enumerate(blocks[indI]):
-						if ind > indJ:
-							blocks[k].append(k)
-							blocks[indI][ind] = None
-
-	RemoveNone(blocks)
-	# Poner a en b
-	blocks[index[0]].append(blocks[indexY[0]][indexY[1]])
-	# quitar el que se movió
-	blocks[indexY[0]].remove(int(y))
-
-def MoveOver(y, x, blocks):
-	index = []
-	indexY = []
-	# Buscar a y b y quitar los del stack de a
-	for indI, i in enumerate(blocks):
-		if len(i) > 0:
-			for indJ, j in enumerate(blocks[indI]):
-				if j == int(y):
-					indexY = [indI, indJ]
-					for ind, k in enumerate(blocks[indI]):
-						if ind > indJ:
-							blocks[k].append(k)
-							blocks[indI][ind] = None
-				if j == int(x):
-					index = [indI, indJ]
-
-	RemoveNone(blocks)
-	# Poner a en b
-	blocks[index[0]].append(blocks[indexY[0]][indexY[1]])
-	# quitar el que se movió
-	blocks[indexY[0]].remove(int(y))
-
-def PileOnto(y, x, blocks):
-	index = []
-	indexY = []
-	# Buscar a y b y quitar los del stack de a y b
-	for indI, i in enumerate(blocks):
-		if len(i) > 0:
-			for indJ, j in enumerate(blocks[indI]):
-				if j == int(x):
-					index = [indI, indJ]
-					for ind, k in enumerate(blocks[indI]):
-						if ind > indJ:
-							blocks[k].append(k)
-							blocks[indI][ind] = None
-				if j == int(y):
-					indexY = [indI, indJ]
-
-	# Poner a en b
-	for ind in range(indexY[1], len(blocks[indexY[0]])):
-		blocks[index[0]].append(blocks[indexY[0]][ind])
-		blocks[indexY[0]][ind] = None
-	# quitar el que se movió
-	RemoveNone(blocks)
-
-def PileOver(y, x, blocks):
-	index = []
-	indexY = []
-	# Buscar a y b y quitar los del stack de a y b
-	for indI, i in enumerate(blocks):
-		if len(i) > 0:
-			for indJ, j in enumerate(blocks[indI]):
-				if j == int(x):
-					index = [indI, indJ]
-				if j == int(y):
-					indexY = [indI, indJ]
-
-	# Poner a en b
-	for ind in range(indexY[1], len(blocks[indexY[0]])):
-		blocks[index[0]].append(blocks[indexY[0]][ind])
-		blocks[indexY[0]][ind] = None
-	# quitar el que se movió
-	RemoveNone(blocks)
-
-def RemoveNone(blocks):
-	# Remover None
-	for i in range(len(blocks)):
-		if len(blocks[i]):
-			for j in range(len(blocks[i])):
-				try:
-					blocks[i].remove(None)
-				except:
-					None
-
-main()
+if __name__ == '__main__':
+    main()
